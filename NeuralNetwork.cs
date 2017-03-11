1f1aa9d0366f1e7573860ebea308740440e6720b
synetic707 <synetic707@DESKTOP-3G1TF9A>
2017-03-11 11:40:15 +0100

    First Upload

---
+++ NeuralNetwork.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
+
+
+namespace SimpleNN_translated
+{
+    public class NeuralNetwork
+    {
+         Matrix<double> synapticWeights;
+
+        public NeuralNetwork()
+        {
+            synapticWeights = createMatrixWithRandomNumbers(3, 1);
+        }
+
+        private Matrix<double> sigmoid(Matrix<double> x, bool derivative)
+        {
+            if (derivative)
+            {
+                return x.PointwiseMultiply(1 - x);
+            }
+            else
+            {
+                return 1 / (1 + (1 / x.PointwiseExp()));
+            }
+        }
+
+        public void Train(double[,] trainingInput, double[,] trainingOutput, int trainingIterations)
+        {
+            var mtrainingInput = buildMatrixFromArray(trainingInput);
+            var mtrainingOutput = buildMatrixFromArray(trainingOutput);
+             
+            for (int i = 0; i < trainingIterations; i++)
+            {
+                var output = Think(trainingInput);
+                var error = mtrainingOutput - output;
+                var adjustment = dotProduct(mtrainingInput.Transpose(), error.PointwiseMultiply(sigmoid(output, true)));
+                synapticWeights += adjustment;
+            }
+        }
+
+        public Matrix<double> Think(double[,] inputs)
+        {
+            var minputs = buildMatrixFromArray(inputs);
+
+            return sigmoid(dotProduct(minputs, synapticWeights), false);
+        }
+
+        private Matrix<double> dotProduct(Matrix<double> matrixOne, Matrix<double> matrixTwo)
+        {
+            return matrixOne * matrixTwo;
+        }
+
+        private Matrix<double> createMatrixWithRandomNumbers(int rowCount, int columnCount)
+        {
+            Random rand = new Random();
+            double[,] matrix = new double[rowCount, columnCount];
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    double maxValue = 1.0;
+                    double minValue = -1.0;
+
+                    double betweenMinusOneToOne = rand.NextDouble() * (maxValue - minValue) + minValue;
+                    matrix[i, j] = betweenMinusOneToOne;
+                }
+            }
+
+            return Matrix<double>.Build.DenseOfArray(matrix);
+        }
+
+        private Matrix<double> buildMatrixFromArray(double[,] array)
+        {
+            return Matrix<double>.Build.DenseOfArray(array);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNN_translated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //3 Input - 1 Output
+            double[,] training_set_inputs = new double[,] { { 0, 0, 1},
+                                                            { 1, 1, 1},
+                                                            { 1, 0, 1},
+                                                            { 0, 1, 1}};
+
+            double[,] training_set_outputs = new double[,] { {0},
+                                                             {1},
+                                                             {1},
+                                                             {0}};
+
+            NeuralNetwork neuralNetwork = new NeuralNetwork();
+
+
+            Console.WriteLine("Start training ...");
+
+            // Train the neural network using a training set.
+            // Do it 10,000 times
+            neuralNetwork.Train(training_set_inputs, training_set_outputs, 10000);
+            Console.WriteLine("End training ...\n\n");
+
+            // Predict
+            Console.WriteLine("Considering new situation [1, 0, 0] -> ?\n");
+            var result = neuralNetwork.Think(new double[,] { { 1, 0, 0 } });
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
